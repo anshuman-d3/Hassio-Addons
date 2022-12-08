13c257277f3e8f8f964d1b3bc4f8f6dbed46b3fe
valux <marco.valvano@live.de>
2022-12-08 22:16:29 +0100

    change config-directory to /share/mopidy

--- mopidy/run.sh
+++ mopidy/run.sh
@@ -11,7 +11,7 @@ mkdir -p /media/mopidy/playlists
 bashio::log.info 'Setup config'
 local_scan=$(cat /data/options.json | jq -r '.local_scan // empty')
 options=$(cat /data/options.json | jq -r 'if .options then [.options[] | "-o "+.name+"="+.value ] | join(" ") else "" end')
-config="/var/lib/mopidy/.config/mopidy/mopidy.conf"
+config="/share/mopidy/mopidy.conf"
 
 bashio::log.info 'Start Mopidy....'
 if  [ "$local_scan" == "true" ]; then
